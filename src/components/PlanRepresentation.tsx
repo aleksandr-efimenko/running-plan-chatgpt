7f0501f267d1d492dfddff9f9ee11a6ec7961feb
Aleksandr Efimenko <androidblog@yandex.ru>
2023-07-08 17:03:30 +0400

    use fullcalendar for showing plan

---
+++ src/components/PlanRepresentation.tsx
@@ -0,0 +1,38 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import multiMonthPlugin from "@fullcalendar/multimonth";
+import listPlugin from "@fullcalendar/list";
+import { type EventSourceInput } from "@fullcalendar/core";
+
+export type RunningPlanData = {
+  date: string;
+  training: string;
+};
+
+export function PlanRepresentation({ plan }: { plan: RunningPlanData[] }) {
+  const myEventsList: EventSourceInput = plan.map((planData) => {
+    return {
+      title: planData.training,
+      date: new Date(planData.date),
+      allDay: true,
+    };
+  });
+  return (
+    <div className="container max-h-screen w-full">
+      <FullCalendar
+        plugins={[dayGridPlugin, multiMonthPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        multiMonthMaxColumns={1}
+        weekends={true}
+        events={myEventsList}
+        eventDisplay="block"
+        eventTimeFormat={{}}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,multiMonthYear,listYear",
+        }}
+      />
+    </div>
+  );
+}
